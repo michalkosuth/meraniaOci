8d38252c1563b97a94c13feacd148f4692e61098
michalkosuth <98960333+michalkosuth@users.noreply.github.com>
2022-02-07 22:36:39 +0100

    initial commit

---
+++ src/main/java/org/mgmk/meraniaOci/MeraniaOciApplication.java
@@ -0,0 +1,13 @@
+package org.mgmk.meraniaOci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeraniaOciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeraniaOciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mgmk/meraniaOci/MeraniaOciApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mgmk.meraniaOci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeraniaOciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
